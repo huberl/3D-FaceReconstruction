53fa8da4542f8dab5a24e0e23063c353bdc6ec90
Tobias Kirschstein <tobias.kirschstein@tum.de>
2021-01-20 09:03:33 +0100

    Implement Alternating Optimization

--- face_reconstruction/optim/sparse.py
+++ face_reconstruction/optim/sparse.py
@@ -33,6 +33,7 @@ class SparseOptimization:
                  bfm: BaselFaceModel,
                  n_params_shape,
                  n_params_expression,
+                 fix_camera_pose=False,
                  weight_shape_params=1.0,
                  weight_expression_params=1.0):
         """
@@ -47,6 +48,8 @@ class SparseOptimization:
             Specifies that only the first `n_params_expression` parameters of the expression model will be optimized for.
             These are the parameters that have the biggest impact on the face model.
             The remaining coefficients will be held constant to 0.
+        :param fix_camera_pose:
+            Whether the camera pose should be optimized for
         :param weight_shape_params:
             Specifies how much more changing a shape coefficient parameter will impact the loss
         :param weight_expression_params:
@@ -55,6 +58,7 @@ class SparseOptimization:
         self.bfm = bfm
         self.n_params_shape = n_params_shape
         self.n_params_expression = n_params_expression
+        self.fix_camera_pose = fix_camera_pose
         self.weight_shape_params = weight_shape_params
         self.weight_expression_params = weight_expression_params
 
@@ -78,21 +82,40 @@ class SparseOptimization:
         return SparseOptimizationParameters.from_theta(self, theta)
 
     def create_parameters(self,
-                          shape_coefficients: np.ndarray,
-                          expression_coefficients: np.ndarray,
-                          camera_pose: np.ndarray):
+                          shape_coefficients: np.ndarray = None,
+                          expression_coefficients: np.ndarray = None,
+                          camera_pose: np.ndarray = None
+                          ):
         return SparseOptimizationParameters(
             self,
             shape_coefficients=shape_coefficients,
             expression_coefficients=expression_coefficients,
             camera_pose=camera_pose)
 
-    def create_loss(self, renderer, bfm_landmark_indices, img_landmark_pixels):
+    def create_parameters_from_other(self, parameters):
+        return SparseOptimizationParameters(
+            self,
+            shape_coefficients=parameters.shape_coefficients,
+            expression_coefficients=parameters.shape_coefficients,
+            camera_pose=parameters.camera_pose
+        )
+
+    def create_loss(self,
+                    renderer,
+                    bfm_landmark_indices,
+                    img_landmark_pixels,
+                    fixed_camera_pose: np.ndarray = None,
+                    fixed_shape_coefficients: np.ndarray = None,
+                    fixed_expression_coefficients: np.ndarray = None
+                    ):
         return SparseOptimizationLoss(
             self,
             renderer=renderer,
             bfm_landmark_indices=bfm_landmark_indices,
-            img_landmark_pixels=img_landmark_pixels)
+            img_landmark_pixels=img_landmark_pixels,
+            fixed_camera_pose=fixed_camera_pose,
+            fixed_shape_coefficients=fixed_shape_coefficients,
+            fixed_expression_coefficients=fixed_expression_coefficients)
 
 
 class SparseOptimizationLoss:
@@ -102,7 +125,10 @@ class SparseOptimizationLoss:
             optimization_manager: SparseOptimization,
             renderer: SimpleImageRenderer,
             bfm_landmark_indices: np.ndarray,
-            img_landmark_pixels: np.ndarray):
+            img_landmark_pixels: np.ndarray,
+            fixed_camera_pose: np.ndarray = None,
+            fixed_shape_coefficients: np.ndarray = None,
+            fixed_expression_coefficients: np.ndarray = None):
         """
         :param optimization_manager:
             the optimization manager that specifies how the optimization should be performed
@@ -118,20 +144,40 @@ class SparseOptimizationLoss:
         self.bfm_landmark_indices = bfm_landmark_indices
         self.img_landmark_pixels = img_landmark_pixels
 
+        assert not optimization_manager.fix_camera_pose or fixed_camera_pose is not None, \
+            "If camera pose is fixed, it has to be provided to the loss"
+        self.fixed_camera_pose = fixed_camera_pose
+
+        self.fixed_shape_coefficients = fixed_shape_coefficients
+        self.fixed_expression_coefficients = fixed_expression_coefficients
+
     def __call__(self, *args, **kwargs):
         return self.loss(args[0], args[1:], kwargs)
 
     def loss(self, theta, *args, **kwargs):
-        # global residuals_before
         parameters = self.optimization_manager.create_parameters_from_theta(theta)
-        # print(parameters.camera_pose)
-        # print(parameters.shape_coefficients)
+
+        if self.fixed_shape_coefficients is None:
+            shape_coefficients = parameters.shape_coefficients
+        else:
+            shape_coefficients = self.fixed_shape_coefficients
+
+        if self.fixed_expression_coefficients is None:
+            expression_coefficients = parameters.expression_coefficients
+        else:
+            expression_coefficients = self.fixed_expression_coefficients
+
+        if self.optimization_manager.fix_camera_pose:
+            camera_pose = self.fixed_camera_pose
+        else:
+            camera_pose = parameters.camera_pose
+
         face_mesh = self.optimization_manager.bfm.draw_sample(
-            shape_coefficients=parameters.shape_coefficients,
-            expression_coefficients=parameters.expression_coefficients,
+            shape_coefficients=shape_coefficients,
+            expression_coefficients=expression_coefficients,
             color_coefficients=[0 for _ in range(self.optimization_manager.n_color_coefficients)])
         landmark_points = np.array(face_mesh.vertices)[self.bfm_landmark_indices]
-        face_landmark_pixels = self.renderer.project_points(parameters.camera_pose, landmark_points)
+        face_landmark_pixels = self.renderer.project_points(camera_pose, landmark_points)
         residuals = face_landmark_pixels - self.img_landmark_pixels
 
         # residuals = np.linalg.norm(residuals, axis=1)
@@ -165,6 +211,13 @@ class SparseOptimizationParameters:
         n_params_shape = optimization_manager.n_params_shape
         n_params_expression = optimization_manager.n_params_expression
 
+        assert shape_coefficients is not None or n_params_shape == 0, "If n_params_shape > 0 then shape coefficients have to be provided"
+        if shape_coefficients is None:
+            shape_coefficients = []
+
+        assert expression_coefficients is not None or n_params_expression == 0, "If n_params_expression > 0 then expression coefficients have to be provided"
+        if expression_coefficients is None:
+            expression_coefficients = []
         # Shape and expression coefficients are multiplied by their weight to enforce that changing them
         # will have a higher impact depending on the weight
         self.shape_coefficients = np.hstack(
@@ -172,11 +225,16 @@ class SparseOptimizationParameters:
              np.zeros((n_shape_coefficients - n_params_shape))])
         self.expression_coefficients = np.hstack([expression_coefficients[:n_params_expression],
                                                   np.zeros((n_expression_coefficients - n_params_expression))])
-        self.camera_pose = camera_pose
+
+        assert camera_pose is not None or optimization_manager.fix_camera_pose, "Camera pose may only be None if it is fixed"
+        if camera_pose is not None:
+            self.camera_pose = camera_pose
 
     @staticmethod
     def from_theta(optimization_manager: SparseOptimization, theta: np.ndarray):
         """
+        :param optimization_manager:
+            Specifies how the optimization should be done
         :param theta:
             Contains a list of parameters that are interpreted as follows.
             The 1st `n_shape_params` are shape coefficients
@@ -191,11 +249,14 @@ class SparseOptimizationParameters:
                                   n_params_shape:n_params_shape + n_params_expression] * optimization_manager.weight_expression_params
         i = n_params_shape + n_params_expression
 
-        # TODO: Enforcing unity at Quaternion does not yet yield desired effect
-        q = Quaternion(*theta[i:i + 4]).unit  # Important that we only allow unit quaternions
-        camera_pose = q.transformation_matrix
-        i = i + 4
-        camera_pose[:3, 3] = theta[i:i + 3]
+        if optimization_manager.fix_camera_pose:
+            camera_pose = None
+        else:
+            # TODO: Enforcing unity at Quaternion does not yet yield desired effect
+            q = Quaternion(*theta[i:i + 4]).unit  # Important that we only allow unit quaternions
+            camera_pose = q.transformation_matrix
+            i = i + 4
+            camera_pose[:3, 3] = theta[i:i + 3]
 
         return SparseOptimizationParameters(
             optimization_manager=optimization_manager,
@@ -211,8 +272,10 @@ class SparseOptimizationParameters:
                      / self.optimization_manager.weight_shape_params)
         theta.extend(self.expression_coefficients[:self.optimization_manager.n_params_expression]
                      / self.optimization_manager.weight_expression_params)
-        q = Quaternion(matrix=self.camera_pose)
-        theta.extend(q)
-        theta.extend(self.camera_pose[:3, 3])
+
+        if not self.optimization_manager.fix_camera_pose:
+            q = Quaternion(matrix=self.camera_pose)
+            theta.extend(q)
+            theta.extend(self.camera_pose[:3, 3])
 
         return np.array(theta)
